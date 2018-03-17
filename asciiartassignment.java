b4c53e319a48ba88ccc2cac2ff8de270f9deb9c1
Mostafa Rizk <m.rizk.student@gmail.com>
2018-03-17 09:19:40 -0600

    Completed Ascii-Art-Challenge

---
+++ asciiartassignment.java
@@ -0,0 +1,11 @@
+public class asciiartassignment
+{
+	public static void main(String args[])
+	{
+		System.out.println("M     M    MMM     MMMM   MMMMM   MM    MMM   MM");
+		System.out.println("MM   MM   M   M   MM        M    M  M   M    M  M");
+		System.out.println("M M M M  M     M   MMMM     M    MMMM   MMM  MMMM");
+		System.out.println("M  M  M   M   M       MM    M    M  M   M    M  M");
+		System.out.println("M     M    MMM     MMMM     M    M  M   M    M  M");
+	}
+}
